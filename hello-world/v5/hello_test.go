f7757d3a8edc3b07fecb30dde9d23f12c0736050
揭思敏 <jsm0834@175game.com>
2018-06-12 20:35:03 +0800

    Hello world: v5 将重复的 assertion 抽象成 assertCorrectMessage
    * 在函数中可以定义一个函数并将其赋给变量
    * 传递 t *testing.T 到 assertCorrectMessage 中以调用 t.fail 等方法
    * t.Helper() 将会使出错时的行数在测试函数而不是 assertCorrectMessage 中

--- hello-world/v5/hello_test.go
+++ hello-world/v5/hello_test.go
@@ -4,24 +4,26 @@ import (
 	"testing"
 )
 
-
 func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
-
+		assertCorrectMessage(t, got, want)
 	})
 }
