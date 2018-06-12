a10c299d8211382ba04b95c8f47382700b4b5b1e
揭思敏 <jsm0834@175game.com>
2018-06-12 20:54:41 +0800

    Hello world: v6 语言增加 French

--- hello-world/v6/hello.go
+++ hello-world/v6/hello.go
@@ -6,6 +6,8 @@ const helloPrefix  = "Hello, "
 const world  = "World"
 const spanish  = "Spanish"
 const spanishHelloPrefix  = "Hola, "
+const french  = "French"
+const frenchHelloPrefix  = "Bonjour, "
 
 func Hello(name, language string) string {
 	if name == "" {
@@ -14,6 +16,8 @@ func Hello(name, language string) string {
 
 	if language == spanish {
 		return spanishHelloPrefix + name
+	} else if language == french {
+		return frenchHelloPrefix + name
 	}
 
 	return helloPrefix + name
