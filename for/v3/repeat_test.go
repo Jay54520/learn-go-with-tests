44cf6242dec9c561420e6f28adb86e34d669db91
揭思敏 <jsm0834@175game.com>
2018-06-13 09:14:58 +0800

    for: v3: test: 增加控制字符重复次数的参数

--- for/v3/repeat_test.go
+++ for/v3/repeat_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T)  {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("expected '%s' but got '%s'", expected, repeated)
