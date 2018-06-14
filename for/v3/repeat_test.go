9158a414c984738057285dd17c398060d5d738eb
揭思敏 <jsm0834@175game.com>
2018-06-14 08:05:59 +0800

    for: v3: test: Add ExampleRepeat

--- for/v3/repeat_test.go
+++ for/v3/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestRepeat(t *testing.T)  {
 	repeated := Repeat("a", 3)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i:=0; i < b.N; i++ {
 		Repeat("a", 0)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("c", 3)
+	fmt.Println(repeated)
+	// Output: ccc
 }
