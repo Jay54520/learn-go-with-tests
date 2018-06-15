4756b74267890abd47a20ab1ae1c1e472852aa41
揭思敏 <jsm0834@175game.com>
2018-06-15 22:36:46 +0800

    structs: v6: refactor: use table driven tests to extract all area tests
    * table driven tests 适用于代码格式相同的测试，这样增加测试会很方便，只需要增加一行代码
    * 使用匿名 struct 创建 areaTests

---
+++ structs/v6/shapes_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct{
+		shape Shape
+		want float64
+	}{
+		{Rectangle{10.0, 5.0}, 50},
+		{Circle{10.0}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+
+}
