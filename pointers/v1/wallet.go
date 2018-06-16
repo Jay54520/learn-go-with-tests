01b6f102138ad773b52c3550b396944ca9c18e3e
揭思敏 <jsm0834@175game.com>
2018-06-16 11:24:02 +0800

    pointers: v1: fix: got 0 want 10
    使用指针作为 Deposit 和 Balance 的参数，这样两者修改的就是同一个对象了。

--- pointers/v1/wallet.go
+++ pointers/v1/wallet.go
@@ -6,11 +6,11 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet)  Deposit(amount int) {
+func (w *Wallet)  Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() (balance int)  {
+func (w *Wallet) Balance() (balance int)  {
 	return w.balance
 }
