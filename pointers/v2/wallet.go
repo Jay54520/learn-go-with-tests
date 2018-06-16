60f325110a361d632217fdbc1baa53c9642e77f0
揭思敏 <jsm0834@175game.com>
2018-06-16 11:31:27 +0800

    pointers: v2: refactor: use BitCoin to replace int
    原因是 BitCoin 的描述性相比 int 更强，而这里使用 struct 的话又太过了，所以创建一个新的 int 类型(别名)。

--- pointers/v2/wallet.go
+++ pointers/v2/wallet.go
@@ -1,13 +1,15 @@
 package main
 
+type BitCoin int
+
 type Wallet struct {
-	balance int
+	balance BitCoin
 }
 
-func (w *Wallet)  Deposit(amount int) {
+func (w *Wallet)  Deposit(amount BitCoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() (balance int)  {
+func (w *Wallet) Balance() (balance BitCoin)  {
 	return w.balance
 }
