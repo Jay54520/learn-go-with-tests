39b51f0ad69057eac20ae5d7495b8e40e73b3313
揭思敏 <jsm0834@175game.com>
2018-06-17 14:22:27 +0800

    mocking: v3: fix: too many arguments in call to CountDown
    have (*bytes.Buffer, *SpySleeper)
    want (io.Writer)

--- mocking/v3/main.go
+++ mocking/v3/main.go
@@ -8,19 +8,19 @@ import (
 )
 
 func main()  {
-	CountDown(os.Stdout)
+	CountDown(os.Stdout, time)
 }
 
 const finalWord  = "Go!"
 const countdownStart  = 3
 
-func CountDown(writer io.Writer)  {
+func CountDown(writer io.Writer, sleeper Sleeper)  {
 	for i := countdownStart; i > 0; i -- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep(1 * time.Second)
 		fmt.Fprintln(writer, i)
 	}
 
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep(1 * time.Second)
 	fmt.Fprintf(writer, finalWord)
 }
 
