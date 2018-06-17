eaa99eedfbfb4613e99f53b52c642528f7977cb4
揭思敏 <jsm0834@175game.com>
2018-06-17 15:05:16 +0800

    mocking: v5: feat: add ConfigurableSleeper struct
    ConfigurableSleeper 会使用 sleep 方法睡眠 duration 的时间。
    
    ConfigurableSleeper.sleep 和 time.Sleep 的函数签名是一样的，使我们可以在生产环境代码中调用 time.Sleep。

---
+++ mocking/v5/countdown_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"testing"
+	"bytes"
+	"reflect"
+)
+
+func TestCountDown(t *testing.T) {
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+
+		CountDown(buffer, &CountdownOperationsSpy{})
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+
+	t.Run("sleep after every print", func(t *testing.T) {
+		spySLeepPrinter := &CountdownOperationsSpy{}
+		CountDown(spySLeepPrinter, spySLeepPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(spySLeepPrinter.Calls, want) {
+			t.Errorf("wanted calls %v got %v", want, spySLeepPrinter.Calls)
+		}
+	})
+}
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+	return
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
---
+++ mocking/v5/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"io"
+	"os"
+	"fmt"
+	"time"
+)
+
+func main()  {
+	sleeper := &DefaultSleeper{}
+	CountDown(os.Stdout, sleeper)
+}
+
+const finalWord  = "Go!"
+const countdownStart  = 3
+
+func CountDown(writer io.Writer, sleeper Sleeper)  {
+	for i := countdownStart; i > 0; i -- {
+		sleeper.Sleep()
+		fmt.Fprintln(writer, i)
+	}
+
+	sleeper.Sleep()
+	fmt.Fprintf(writer, finalWord)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct {
+
+}
+
+func (d *DefaultSleeper) Sleep()  {
+	time.Sleep(1 * time.Second)
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep func(duration time.Duration)
+}
