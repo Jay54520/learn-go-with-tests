3adce2518ef2324eff96861f3673f8bd5b8ad686
揭思敏 <jsm0834@175game.com>
2018-06-18 19:46:11 +0800

    select: v3: test: add test "returns an error if a server doesn't respond within 10 secs"

---
+++ select/v3/racer_test.go
@@ -0,0 +1,47 @@
+package racer
+
+import (
+	"testing"
+	"net/http/httptest"
+	"net/http"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+
+	t.Run("returns an error if a server doesn't respond within 10 secs", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
