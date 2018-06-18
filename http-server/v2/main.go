15370a570afe8f8473db1b3b9418bfeb6fef6be2
揭思敏 <jsm0834@175game.com>
2018-06-18 21:41:17 +0800

    http-server: v2: feat: add application code
    "http.HandlerFunc" 是一个将普通函数转换为 HTTP handlers 的适配器。
    "http.ListenAndServe" 监听一个端口并将请求交给处理器，如果端口已经被占用，那么
    返回错误的错误非空。

---
+++ http-server/v2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+func main()  {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
