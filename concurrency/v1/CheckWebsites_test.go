455fc6e223992419befc22170354baed989eb157
揭思敏 <jsm0834@175game.com>
2018-06-18 16:02:40 +0800

    concurrency: v1: refactor: 使用 goroutine 运行 WebsiteChecker
    与其在发送请求前等待之前的网站响应，我们让请求同步进行（当之前的请求还在等待）。通过
    goroutine 实现这点。通过在函数调用前增加 "go" 来启动 goroutine。
    
    以上引入了匿名函数。相对于普通函数定义，没有函数的名字，并且可以在定义时就立即运行。

---
+++ concurrency/v1/CheckWebsites_test.go
@@ -0,0 +1,33 @@
+package concurrency
+
+import (
+	"testing"
+	"reflect"
+)
+
+func mockWebsiteChecker(url string) bool  {
+	if url == "http://fail.com" {
+		return false
+	}
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"http://fail.com",
+	}
+
+	actualResults := CheckWebsites(mockWebsiteChecker, websites)
+
+	expectedResults := map[string]bool{
+		"http://google.com": true,
+		"http://blog.gypsydave5.com": true,
+		"http://fail.com": false,
+	}
+
+	if !reflect.DeepEqual(expectedResults, actualResults) {
+		t.Fatalf("Wanted %v, got %v", expectedResults, actualResults)
+	}
+}
