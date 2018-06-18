b451872a95d7c8a7624068d6443c1f0c19c62094
揭思敏 <jsm0834@175game.com>
2018-06-19 07:47:57 +0800

    http-server: v2: refactor: 抽象出 GetPlayerScore

--- http-server/v2/server.go
+++ http-server/v2/server.go
@@ -8,10 +8,14 @@ import (
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 
-	if player == "Pepper" {
-		fmt.Fprint(w, "20")
-	} else if player == "Floyd" {
-		fmt.Fprint(w, "10")
-	}
+	fmt.Fprint(w, GetPlayerScore(player))
+}
 
+func GetPlayerScore(player string) (score string) {
+	if player == "Pepper" {
+		score = "20"
+	} else if player == "Floyd" {
+		score = "10"
+	}
+	return
 }
