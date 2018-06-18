f4b9ad3b3cd847d65902627a648bc369332535dc
揭思敏 <jsm0834@175game.com>
2018-06-19 07:45:30 +0800

    http-server: v2: fix: got '20', want '10'
    新增的 "returns Floyd's score" 测试强迫我们去查看请求的 URL 并据此做决定。
    
    如果我们之前将分数储存弄的复杂，那么这里就要改动很多部分了。这就是使用小步前进的原因。
    
    我们正在抵抗使用任何路由库的诱惑，只是为了小步使测试通过。目前通过 slice 获取路径不够 robust，但对目前的情况来说足够了。

--- http-server/v2/server.go
+++ http-server/v2/server.go
@@ -5,6 +5,13 @@ import (
 	"fmt"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "20")
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+	} else if player == "Floyd" {
+		fmt.Fprint(w, "10")
+	}
+
 }
