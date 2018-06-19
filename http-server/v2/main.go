416cdfed4d3881e5ff9a225e58a437b03757ef6f
揭思敏 <jsm0834@175game.com>
2018-06-19 08:30:36 +0800

    http-server: v2: feat: add InMemoryPlayerStore
    先 hard-coded 使应用能正常运行。

--- http-server/v2/main.go
+++ http-server/v2/main.go
@@ -5,8 +5,17 @@ import (
 	"log"
 )
 
+type InMemoryPlayerStore struct {
+
+}
+
+func (i InMemoryPlayerStore) GetPlayerScore(name string) (score int)  {
+	score = 123
+	return
+}
+
 func main()  {
-	handler := &PlayerServer{}
+	handler := &PlayerServer{&InMemoryPlayerStore{}}
 	if err := http.ListenAndServe(":5000", handler); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
