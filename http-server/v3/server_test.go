84b4e9595d4fb2813cd47c0f348fa51248541dd2
揭思敏 <jsm0834@175game.com>
2018-06-19 09:09:16 +0800

    http-server: v3: test: add TestStoreWins

--- http-server/v3/server_test.go
+++ http-server/v3/server_test.go
@@ -44,6 +44,22 @@ func TestGETPlayers(t *testing.T) {
 	})
 }
 
+func TestStoreWins(t *testing.T)  {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+}
+
 func assertStatus(t *testing.T, got int, want int) {
 	if got != want {
 		t.Errorf("got status %d want %d", got, want)
