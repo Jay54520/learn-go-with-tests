9a42be7271a6d34e5c4b4f622aa5fb15e3140043
揭思敏 <jsm0834@175game.com>
2018-06-19 09:16:47 +0800

    http-server: v3: refactor: 整理 ServeHTTP 的代码结构
    不是等到所有的功能完成再重构，当所有测试通过后就可以是重构的时机。
    早重构比晚重构好。

--- http-server/v3/server.go
+++ http-server/v3/server.go
@@ -14,11 +14,21 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return 
-	}
 
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
+	return
+
+}
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
 	score := p.store.GetPlayerScore(player)
 
@@ -27,4 +37,5 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, score)
+
 }
