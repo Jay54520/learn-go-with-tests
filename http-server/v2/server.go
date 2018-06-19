2ae6824af16458581cac77f5813f328db5bcfe5c
揭思敏 <jsm0834@175game.com>
2018-06-19 08:05:23 +0800

    http-server: v2: refactor: 将 PlayerServer 替换为 struct
    这样 PlayerServer struct 就可以直接调用 struct 中的 store 而不需要通过函数调用

--- http-server/v2/server.go
+++ http-server/v2/server.go
@@ -5,12 +5,6 @@ import (
 	"fmt"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
-
-	fmt.Fprint(w, GetPlayerScore(player))
-}
-
 func GetPlayerScore(player string) (score string) {
 	if player == "Pepper" {
 		score = "20"
@@ -22,4 +16,14 @@ func GetPlayerScore(player string) (score string) {
 
 type PlayerStore interface {
 	GetPlayerScore(player string) (score int)
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
